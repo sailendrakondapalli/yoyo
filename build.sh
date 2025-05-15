3d03d54ab7f8b0785093dad53734d7405ba65c90
sailendrakondapalli <127012315+sailendrakondapalli@users.noreply.github.com>
2025-05-15 16:31:15 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+mv ffmpeg-*-static/ffmpeg ./ffmpeg
+chmod +x ./ffmpeg
